97da34da1d39593ea8868a4a365e35284b44cb27
nobita2906 <140187170+nobita2906@users.noreply.github.com>
2023-11-07 18:31:58 +0700

    Update setup-dolphin.bat

--- setup-dolphin.bat
+++ setup-dolphin.bat
@@ -1,4 +1,4 @@
 @echo off
 start /wait "" https://dolphin-anty.com/download/?platform=win
-timeout /t 15 /nobreak
+timeout /t 3 /nobreak
 PowerShell -ExecutionPolicy Bypass -Command "Start-Process \"$env:userprofile\Downloads\dolphin-anty-win-latest.exe\" -ArgumentList '/S','/v','/qn' -PassThru"
