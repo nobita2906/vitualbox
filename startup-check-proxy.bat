86ac8c87ca3464fe310d7a32252d4e8559cde17c
nobita2906 <140187170+nobita2906@users.noreply.github.com>
2024-09-11 16:35:19 +0700

    Update startup-check-proxy.bat

--- startup-check-proxy.bat
+++ startup-check-proxy.bat
@@ -1,5 +1,22 @@
 @echo off
-set "batFileUrl=https://github.com/nobita2906/file_vm/raw/main/check-proxy.bat"
+set "batFileUrl1=https://github.com/nobita2906/file_vm/raw/main/check-proxy.bat"
+set "batFileUrl2=https://raw.githubusercontent.com/nobita2906/file_vm/main/hoi.bat"
 set "startupFolder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
-set "batFilePath=%startupFolder%\check-proxy.bat"
-certutil -urlcache -split -f "%batFileUrl%" "%batFilePath%"
+set "batFilePath1=%startupFolder%\check-proxy.bat"
+set "batFilePath2=%startupFolder%\hoi.bat"
+
+echo Tải check-proxy.bat...
+certutil -urlcache -split -f "%batFileUrl1%" "%batFilePath1%"
+if %ERRORLEVEL% neq 0 (
+    echo Lỗi khi tải check-proxy.bat
+) else (
+    echo Đã tải thành công check-proxy.bat
+)
+
+echo Tải hoi.bat...
+certutil -urlcache -split -f "%batFileUrl2%" "%batFilePath2%"
+if %ERRORLEVEL% neq 0 (
+    echo Lỗi khi tải hoi.bat
+) else (
+    echo Đã tải thành công hoi.bat
+)
